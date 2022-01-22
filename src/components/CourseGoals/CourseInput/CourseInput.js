926b9ab236b984f5f316d0d08d24f602c573f9b9
anant mehra <anant0007mehra@gmail.com>
2022-01-22 17:36:41 +0530

    Empty Goal Feedback

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,6 +5,7 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] =useState(true); 
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
@@ -12,13 +13,18 @@ const CourseInput = props => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    // we need to check whether something valid was enterd or not / feedback too
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      return; // cant add empty goal
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>To Do :</label>
+        <label style={{ color: !isValid ? 'red' : 'black' }}>To Do :</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Task</Button>
